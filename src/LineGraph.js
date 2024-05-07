547b512b8f0fd1429f6a8dee8236bec395fd34ae
Sindhoora Ponnuswamy <sindhoorap.30@gmail.com>
2024-05-07 15:01:43 +0100

    updated the graph

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -31,6 +31,7 @@ const LineGraph = () => {
               borderWidth: 1.5,
               backgroundColor: 'rgba(75, 192, 192, 0.1)',
               yAxisID: 'y1',
+              pointRadius: 0, 
             },
             {
               label: 'Concurrency',
@@ -39,15 +40,16 @@ const LineGraph = () => {
               borderWidth: 1.5,
               backgroundColor: 'rgba(255, 99, 132, 0.1)',
               yAxisID: 'y',
+              pointRadius: 0, 
             },
           ],
         },
-        options: {
+        options: { 
           scales: {
             x: {
               title: {
                 display: true,
-                text: 'Time (min)'
+                text: 'Time'
               },
               ticks: {
                 autoSkip: true,
@@ -98,17 +100,29 @@ const formatTime = (time) => {
   // Extracting hours, minutes, and seconds from the time string
   const [hours, minutes, seconds] = time.split(":");
   
-  console.log("Hours:", hours);
-  console.log("Minutes:", minutes);
-  console.log("Seconds:", seconds);
+  // console.log("Hours:", hours);
+  // console.log("Minutes:", minutes);
+  // console.log("Seconds:", seconds);
   
   // Returning hours, minutes, and seconds
   return `${hours}:${minutes}:${seconds}`;
 };
 
   return (
-    <div>
-      <h2>Line Graph</h2>
+    <div
+      style={{
+      // width: '717.71px',
+      // height: '233.33px',
+      // position: 'absolute',
+      // top: '248px',
+      // left: '201px',
+      // borderRadius: '11.67px',
+      //background: '#F7F9FB',
+      //textAlign: 'center', 
+      //paddingTop: '20px' 
+    }}>
+      <h3>Data For Summary Report</h3>
+      
       <canvas id="lineChart" width="800" height="400"></canvas>
     </div>
   );
